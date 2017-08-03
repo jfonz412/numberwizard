561757c7a355a89502aae2b832a3819484ef2cba
James Fonzone <jfonzone@outlook.com>
2017-08-02 20:37:42 -0400

    finish number wizard

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,26 +2,54 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-
+	int max;
+	int min;
+	int guess;
+	
 	// Use this for initialization
 	void Start () {
+		StartGame();
+	}
+	
+	void StartGame()
+	{
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		print("=========================");
 		print("Welcome to Number Wizard");
 		print("Pick a number in your head, but don't tell me");
-		
-		int max = 1000;
-		int min = 1;
 		
 		print("The highest number you can pick is " + max);
 		print("The lowest number you can pick is " + min);
 		
-		print("Is your number higher or lower than 500?");
+		print("Is your number higher or lower than " + guess + "?");
 		print("Press the Up arrow for higher");
 		print("Press the Down arrow for lower");
 		print("Press enter for equals");
+		
+		max = max + 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			//print("Up arrow pressed");
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			//print("Down arrow pressed");	
+			max = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");	
+			StartGame();
+		}
+	}
 	
+	void NextGuess(){
+		guess = (max + min) / 2; //halfway between the max and the min
+		print("Higher or lower than " + guess + "?");
 	}
 }
